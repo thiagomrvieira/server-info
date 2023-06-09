65a9d36e818f39073bf402ec9abda7a4dbef8500
Thiago Vieira <tmaciel@digital-works.com>
2023-06-09 15:52:38 +0100

    feat: Add ServerList component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,10 @@ import './bootstrap';
 
 import { createApp } from 'vue';
 
+import ServerList from "./components/ServerList.vue";
+
 const app = createApp();
+
+app.component('server-list', ServerList);
 
 app.mount("#app");
---
+++ resources/js/components/ServerList.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      <h1>Server List</h1>
+      <ul>
+        <li v-for="server in servers" :key="server.model">
+          <h2>{{ server.model }}</h2>
+          <p>RAM: {{ server.ram.capacity }} ({{ server.ram.type }})</p>
+          <p>HDD: {{ server.hdd.capacity }} ({{ server.hdd.type }})</p>
+          <p>Location: {{ server.location.name }} ({{ server.location.code }})</p>
+          <p>Price: {{ server.price }}</p>
+        </li>
+      </ul>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    data() {
+      return {
+        servers: [],
+      };
+    },
+    mounted() {
+      this.fetchServers();
+    },
+    methods: {
+      fetchServers() {
+        fetch('/api/v1/servers')
+            .then(response => response.json())
+            .then(data => {
+                this.servers = data.data;
+            })
+            .catch(error => {
+                console.error('Error retrieving server data:', error);
+            });
+        },
+    },
+};
+</script>
