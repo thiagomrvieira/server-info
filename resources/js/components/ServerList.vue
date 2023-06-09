3464ab2e2d6cd092671832e4c460c1dc6fd7230e
Thiago Vieira <tmaciel@digital-works.com>
2023-06-09 16:02:26 +0100

    feat: Add refactoring in ServerList component for improved code organization and maintainability

--- resources/js/components/ServerList.vue
+++ resources/js/components/ServerList.vue
@@ -1,39 +1,38 @@
 <template>
     <div>
-      <h1>Server List</h1>
-      <ul>
-        <li v-for="server in servers" :key="server.model">
-          <h2>{{ server.model }}</h2>
-          <p>RAM: {{ server.ram.capacity }} ({{ server.ram.type }})</p>
-          <p>HDD: {{ server.hdd.capacity }} ({{ server.hdd.type }})</p>
-          <p>Location: {{ server.location.name }} ({{ server.location.code }})</p>
-          <p>Price: {{ server.price }}</p>
-        </li>
-      </ul>
+        <h1>Server List</h1>
+        <ul>
+            <li v-for="server in servers" :key="server.model">
+                <h2>{{ server.model }}</h2>
+                <p>RAM: {{ server.ram.capacity }} ({{ server.ram.type }})</p>
+                <p>HDD: {{ server.hdd.capacity }} ({{ server.hdd.type }})</p>
+                <p>Location: {{ server.location.name }} ({{ server.location.code }})</p>
+                <p>Price: {{ server.price }}</p>
+            </li>
+        </ul>
     </div>
-  </template>
+</template>
 
-  <script>
-  export default {
-    data() {
-      return {
-        servers: [],
-      };
-    },
-    mounted() {
-      this.fetchServers();
-    },
-    methods: {
-      fetchServers() {
-        fetch('/api/v1/servers')
-            .then(response => response.json())
-            .then(data => {
-                this.servers = data.data;
-            })
-            .catch(error => {
-                console.error('Error retrieving server data:', error);
-            });
+<script>
+    export default {
+        data() {
+            return {
+                servers: [],
+            };
         },
-    },
-};
+        mounted() {
+            this.fetchServers();
+        },
+        methods: {
+            async fetchServers() {
+                try {
+                    const response = await fetch('/api/v1/servers');
+                    const data = await response.json();
+                    this.servers = data.data;
+                } catch (error) {
+                    console.error('Error retrieving server data:', error);
+                }
+            },
+        },
+    };
 </script>
